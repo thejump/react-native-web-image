bc3b16166d973242fe0f065c17f6a1a7f5eb2103
jcharbo <jcharbo@users.noreply.github.com>
2017-09-18 11:56:17 -0500

    Update WebImagePackage.java

--- android/src/main/java/org/vovkasm/WebImage/WebImagePackage.java
+++ android/src/main/java/org/vovkasm/WebImage/WebImagePackage.java
@@ -16,11 +16,7 @@ public class WebImagePackage implements ReactPackage {
         return Collections.emptyList();
     }
 
-    @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
+   
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.<ViewManager>singletonList(new WebImageViewManager());
